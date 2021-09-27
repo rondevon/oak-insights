af2549bb5d5f010a5a5dd38049f773c1decde3b2
Anusmrity Dolai <anusmritydolai1995@gmail.com>
2021-09-27 20:43:47 +0530

    create share & dashboard

---
+++ src/app/share/share.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShareRoutingModule } from './share-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    ShareRoutingModule
+  ]
+})
+export class ShareModule { }
