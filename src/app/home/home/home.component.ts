6e1c2a025eea25faf0af65bd0c763919c6479936
Anusmrity Dolai <anusmritydolai1995@gmail.com>
2021-09-30 12:00:53 +0530

    remove the background colour added image url added mat-card- (title+ subtitle)

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cards: any[] = [
-    { image: '/assets/icons/icon-energy-intensity.svg', title: 'Energy Usage', value: '10,067', unit: 'kWh' },
-    { image: '/assets/icons/icon-closed-hours.svg', title: 'Closed-hours Consumption', value: '2,044', unit: 'kWh' },
+    { image: '/assets/icons/Icon-Energy-Usage.svg', title: 'Energy Usage', value: '10,067', unit: 'kWh', color: ''},
     { image: '/assets/icons/icon-co2-emission.svg', title: 'CO2 Emission(kg)', value: '2,517', unit: 'kg' },
+    { image: '/assets/icons/icon-closed-hours.svg', title: 'Closed-hours Consumption', value: '2,044', unit: 'kWh' },
     { image: '/assets/icons/icon-energy-intensity.svg', title: 'Energy Intensity ', value: '2517', unit: 'kWh/m2' },
     { image: '/assets/icons/icon-offset-planting.svg', title: 'Offset by Planting', value: '467', unit: 'Oak Trees' },
     { image: '/assets/icons/icon-trend.svg', title: 'Trend vs Last Month', value: '87.5', unit: '%' }
