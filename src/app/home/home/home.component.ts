c3315b46659778b1991acd11df49c1b365c9bfeb
Anusmrity Dolai <anusmritydolai1995@gmail.com>
2021-10-01 12:59:08 +0530

    avenir-black + roman added

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cards: any[] = [
-    { image: '/assets/icons/Icon-Energy-Usage.svg', title: 'Energy Usage', value: '10,067', unit: 'kWh', color: ''},
-    { image: '/assets/icons/icon-co2-emission.svg', title: 'CO2 Emission(kg)', value: '2,517', unit: 'kg' },
-    { image: '/assets/icons/icon-closed-hours.svg', title: 'Closed-hours Consumption', value: '2,044', unit: 'kWh' },
-    { image: '/assets/icons/icon-energy-intensity.svg', title: 'Energy Intensity ', value: '2517', unit: 'kWh/m2' },
-    { image: '/assets/icons/icon-offset-planting.svg', title: 'Offset by Planting', value: '467', unit: 'Oak Trees' },
-    { image: '/assets/icons/icon-trend.svg', title: 'Trend vs Last Month', value: '87.5', unit: '%' }
+    { image: '/assets/icons/Icon-Energy-Usage.svg', title: 'Energy Usage', value: '10,067', unit: 'kWh', color: 'var(--color5)'},
+    { image: '/assets/icons/icon-co2-emission.svg', title: 'CO2 Emission(kg)', value: '2,517', unit: 'kg', color: 'var(--color6)'},
+    { image: '/assets/icons/icon-closed-hours.svg', title: 'Closed-hours Consumption', value: '2,044', unit: 'kWh', color: 'var(--color8)'},
+    { image: '/assets/icons/icon-energy-intensity.svg', title: 'Energy Intensity ', value: '2517', unit: 'kWh/m2',color: 'var(--color5)' },
+    { image: '/assets/icons/icon-offset-planting.svg', title: 'Offset by Planting', value: '467', unit: 'Oak Trees',color: 'var(--color5)' },
+    { image: '/assets/icons/icon-trend.svg', title: 'Trend vs Last Month', value: '87.5', unit: '%',color: 'var(--color7)' }
   ];
 
   constructor() { }
