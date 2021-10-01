3f9d29bdc2c9ff0daad4ad2e9f45120353524038
Anusmrity Dolai <anusmritydolai1995@gmail.com>
2021-10-01 19:06:02 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { DateSuffixPipe } from './date-suffix.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DateSuffixPipe,
 
   ],
   imports: [
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -3,21 +3,64 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   cards: any[] = [
-    { image: '/assets/icons/Icon-Energy-Usage.svg', title: 'Energy Usage', value: '10,067', unit: 'kWh', color: 'var(--color5)'},
-    { image: '/assets/icons/icon-co2-emission.svg', title: 'CO2 Emission(kg)', value: '2,517', unit: 'kg', color: 'var(--color6)'},
-    { image: '/assets/icons/icon-closed-hours.svg', title: 'Closed-hours Consumption', value: '2,044', unit: 'kWh', color: 'var(--color8)'},
-    { image: '/assets/icons/icon-energy-intensity.svg', title: 'Energy Intensity ', value: '2517', unit: 'kWh/m2',color: 'var(--color5)' },
-    { image: '/assets/icons/icon-offset-planting.svg', title: 'Offset by Planting', value: '467', unit: 'Oak Trees',color: 'var(--color5)' },
-    { image: '/assets/icons/icon-trend.svg', title: 'Trend vs Last Month', value: '87.5', unit: '%',color: 'var(--color7)' }
+    {
+      image: '/assets/icons/Icon-Energy-Usage.svg',
+      title: 'Energy Usage',
+      value: '10,067',
+      unit: 'kWh',
+      color: 'var(--color5)',
+    },
+    {
+      image: '/assets/icons/icon-co2-emission.svg',
+      title: 'CO2 Emission(kg)',
+      value: '2,517',
+      unit: 'kg',
+      color: 'var(--color6)',
+    },
+    {
+      image: '/assets/icons/icon-closed-hours.svg',
+      title: 'Closed-hours Consumption',
+      value: '2,044',
+      unit: 'kWh',
+      color: 'var(--color8)',
+    },
+    {
+      image: '/assets/icons/icon-energy-intensity.svg',
+      title: 'Energy Intensity ',
+      value: '2517',
+      unit: 'kWh/m2',
+      color: 'var(--color5)',
+    },
+    {
+      image: '/assets/icons/icon-offset-planting.svg',
+      title: 'Offset by Planting',
+      value: '467',
+      unit: 'Oak Trees',
+      color: 'var(--color5)',
+    },
+    {
+      image: '/assets/icons/icon-trend.svg',
+      title: 'Trend vs Last Month',
+      value: '87.5',
+      unit: '%',
+      color: 'var(--color7)',
+    },
   ];
 
-  constructor() { }
+  events: any[] = [
+    { list: new Date(), name: 'Spring Bank Holiday' },
+    { list: new Date(), name: 'Spring Bank Holiday' },
+    { list: new Date(), name: 'Spring Bank Holiday' },
+    { list: new Date(), name: 'Spring Bank Holiday' },
+  ];
 
-  ngOnInit(): void {
-  }
+  num = 92;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
