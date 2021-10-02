3d8880621d050b9c0d433638939a27b19812f3dc
Nirmalya <im.nirmalyadhar@gmail.com>
2021-10-02 21:01:45 +0530

    Additional Widgets

---
+++ src/app/home/appliance-consumption/appliance-consumption.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appliance-consumption',
+  templateUrl: './appliance-consumption.component.html',
+  styleUrls: ['./appliance-consumption.component.scss']
+})
+export class ApplianceConsumptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
