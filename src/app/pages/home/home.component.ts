3903b88a6fba18a6f387ca78e50c3568c1ae7d57
Anusmrity Dolai <anusmritydolai1995@gmail.com>
2021-10-06 19:40:22 +0530

    homecomponent = (added month dropdown)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,6 +10,7 @@ import { ApiService } from 'src/app/services/api.service';
 export class HomeComponent implements OnInit {
   
   cards: any[] = [];
+  month: string = 'January'
 
   heatmapranges: any[] = [
     {range:'100-200',color:'var(--heatmap-color1)'},
@@ -49,55 +50,62 @@ export class HomeComponent implements OnInit {
       if (data.data && data.data.length > 0) {
         this.weatherData = data.data[0];      
       }
-      this.apiService.getHomepageApi('January').subscribe((data: any) => {
-        this.consumptionData = data.data.consumption_overview;
-        this.oakScore = data.data.oak_score;
-        this.cards = [
-          {
-            image: '/assets/icons/Icon-Energy-Usage.svg',
-            title: 'Energy Usage',
-            value: data.data.stats.energy,
-            unit: 'kWh',
-            color: 'var(--color5)',
-          },
-          {
-            image: '/assets/icons/icon-co2-emission.svg',
-            title: 'CO2 Emission(kg)',
-            value: data.data.stats.co2_emission,
-            unit: 'kg',
-            color: 'var(--color6)',
-          },
-          {
-            image: '/assets/icons/icon-closed-hours.svg',
-            title: 'Closed-hours',
-            value: data.data.stats.closed_hour_energy,
-            unit: 'kWh',
-            color: 'var(--color8)',
-          },
-          {
-            image: '/assets/icons/icon-energy-intensity.svg',
-            title: 'Energy Intensity ',
-            value: data.data.stats.energy_intensity,
-            unit: 'kWh/m2',
-            color: 'var(--color5)',
-          },
-          {
-            image: '/assets/icons/icon-offset-planting.svg',
-            title: 'Offset by Planting',
-            value: data.data.stats.offset_planting,
-            unit: 'Oak Trees',
-            color: 'var(--color5)',
-          },
-          {
-            image: '/assets/icons/icon-trend.svg',
-            title: 'Trend vs Last Month',
-            value: data.data.stats.trend_over_last,
-            unit: '%',
-            color: 'var(--color7)',
-          },
-        ];      
-      })
+      this.updateMonth();
     })
-
+  }
+  
+  updateMonth() {
+    // console.log(this.month);
+    this.apiService.getHomepageApi(this.month).subscribe((data: any) => {
+      console.log(data);
+      
+      this.consumptionData = data.data.consumption_overview;
+      this.oakScore = data.data.oak_score;
+      this.cards = [
+        {
+          image: '/assets/icons/Icon-Energy-Usage.svg',
+          title: 'Energy Usage',
+          value: data.data.stats.energy,
+          unit: 'kWh',
+          color: 'var(--color5)',
+        },
+        {
+          image: '/assets/icons/icon-co2-emission.svg',
+          title: 'CO2 Emission(kg)',
+          value: data.data.stats.co2_emission,
+          unit: 'kg',
+          color: 'var(--color6)',
+        },
+        {
+          image: '/assets/icons/icon-closed-hours.svg',
+          title: 'Closed-hours',
+          value: data.data.stats.closed_hour_energy,
+          unit: 'kWh',
+          color: 'var(--color8)',
+        },
+        {
+          image: '/assets/icons/icon-energy-intensity.svg',
+          title: 'Energy Intensity ',
+          value: data.data.stats.energy_intensity,
+          unit: 'kWh/m2',
+          color: 'var(--color5)',
+        },
+        {
+          image: '/assets/icons/icon-offset-planting.svg',
+          title: 'Offset by Planting',
+          value: data.data.stats.offset_planting,
+          unit: 'Oak Trees',
+          color: 'var(--color5)',
+        },
+        {
+          image: '/assets/icons/icon-trend.svg',
+          title: 'Trend vs Last Month',
+          value: data.data.stats.trend_over_last,
+          unit: '%',
+          color: 'var(--color7)',
+        },
+      ];      
+    })
+    
   }
 }
