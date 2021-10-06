b79cfd4da8a140e7e189c47a6e7342ca4c0e2776
Nirmalya <im.nirmalyadhar@gmail.com>
2021-10-06 19:28:03 +0530

    Landing page changes

--- src/app/dashboard/landing/landing.component.ts
+++ src/app/dashboard/landing/landing.component.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  oakScore = 92;
+
+  cards: any[] = [
+    {
+      image: '/assets/icons/Icon-Energy-Usage.svg',
+      title: 'Energy Usage',
+      value: '10,067',
+      unit: 'kWh',
+      color: 'var(--color5)',
+    },
+    {
+      image: '/assets/icons/icon-co2-emission.svg',
+      title: 'CO2 Emission(kg)',
+      value: '2,517',
+      unit: 'kg',
+      color: 'var(--color6)',
+    },
+    {
+      image: '/assets/icons/icon-closed-hours.svg',
+      title: 'Closed-hours',
+      value: '2,044',
+      unit: 'kWh',
+      color: 'var(--color8)',
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
