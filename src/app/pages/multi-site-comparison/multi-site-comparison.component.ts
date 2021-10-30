2e59fce8813502168ea4c650ba319820cea0cefc
Nirmalya <im.nirmalyadhar@gmail.com>
2021-10-30 19:13:14 +0530

    Table changes

--- src/app/pages/multi-site-comparison/multi-site-comparison.component.ts
+++ src/app/pages/multi-site-comparison/multi-site-comparison.component.ts
@@ -13,8 +13,79 @@ export class MultiSiteComparisonComponent implements OnInit {
     month: this.pipe.transform(new Date(), 'MMMM'),
     year: this.pipe.transform(new Date(), 'YYYY'),
   };
-  siteComparisonData: any = {};
+  siteComparisonData: any [] = [];
   totalSize: any = {total:'1vw',text:'0.7vw',y:'-17'};
+
+  basicInsightLabels: any[] = [{
+    name: 'Energy Supplier',
+    key: 'electric_provider',
+    secondaryKey: 'name'
+  },
+  {
+    name: 'Energy Usage (kWh)',
+    key: 'stats',
+    secondaryKey: 'energy'
+  },
+  {
+    name: 'CO2 Emission (kg)',
+    key: 'stats',
+    secondaryKey: 'co2_emission'
+  },
+  {
+    name: 'Enery Cost (€)',
+    key: 'stats',
+    secondaryKey: 'cost'
+  }, 
+  {
+    name: 'Peak Usage (kWh)',
+    key: 'stats',
+    secondaryKey: 'peak',
+    tertiaryKey: 'energy'
+  },
+  {
+    name: 'Peak Time',
+    key: 'stats',
+    secondaryKey: 'peak',
+    tertiaryKey: 'time'
+  }
+];
+
+  informationLabels: any[] = [{
+    name: 'Area (sq.m)',
+    key: 'area',
+  },
+  {
+    name: 'No. of Employees',
+    key: 'employees'
+  },
+  {
+    name: 'No of Appliances',
+    key: 'appliances'
+  },
+  {
+    name: 'No of Sensors',
+    key: 'devices_count'
+  },
+  {
+    name: 'Operating Hours Consumption',
+    key: 'stats',
+    secondaryKey: 'operating_energy'
+  },
+  {
+    name: 'Non Operating Hours Consumption',
+    key: 'stats',
+    secondaryKey: 'non_operating_energy'
+  },
+  {
+    name: 'Preparatory Hours Consumption',
+    key: 'stats',
+    secondaryKey: 'preparatory_energy'
+  },
+  {
+    name: 'Closed Hours Consumption',
+    key: 'stats',
+    secondaryKey: 'closed_energy'
+  }];
 
   constructor(private apiService: ApiService) { }
 
