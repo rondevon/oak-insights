0c36ce5178ffc76a956ed02de8ee4722bb8e5e85
Nirmalya <im.nirmalyadhar@gmail.com>
2021-12-10 13:49:04 +0530

    recommend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
-export const serviceBaseUrl = `http://18.169.122.251:9010/`;
+export const serviceBaseUrl = `https://api.oak-insights.com/`;
 
 export const environment = {
   NewsApi: 'https://gnews.io/api/v4/search?q=energy+sustainability&token=6cf3e4b0d8356ddd9768dd71b41a01d6',
   WeatherApi:'https://api.weatherbit.io/v2.0/current?',
-  WeatherKey: '&key=59cacdb4dc6845ed92ec5b45ac97123d',
+  WeatherKey: '&key=088cc6116e1c4c61aaf867e91a967374',
   production: true
 };
