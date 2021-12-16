c37f73afedd6916f88b5d1b359f8efc49bf91e2d
Anusmrity Dolai <anusmritydolai1995@gmail.com>
2021-12-16 18:13:18 +0530

    admin for sites and site manager for site

--- src/app/pages/myprofile/myprofile.component.ts
+++ src/app/pages/myprofile/myprofile.component.ts
@@ -13,9 +13,13 @@ export class MyprofileComponent implements OnInit, AfterViewInit {
   data: any;
   site: any;
   dataSource: MatTableDataSource<any> = new MatTableDataSource();
+  dataSource2: MatTableDataSource<any> = new MatTableDataSource();
+  @ViewChild('paginator2') paginator2: any;
   @ViewChild('paginator') paginator: any;
   @ViewChild(MatSort) sort: any;
   displayedColumns: string[] = ['site_id', 'day', 'start', 'end', 'type'];
+  displayedColumns2: string[] = ['site_id', 'electric_provider_id', 'start', 'end', 'cost'];
+  sites: any;
 
 
   constructor(private apiService: ApiService) { }
@@ -28,6 +32,9 @@ export class MyprofileComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.apiService.getProfile().subscribe((data: any) => {
      this.data = data.data;
+     console.log(data.data);
+     
+     this.sites = this.data?.sites ? this.data.sites : this.data.site
     });
   }
 
@@ -40,6 +47,11 @@ export class MyprofileComponent implements OnInit, AfterViewInit {
       this.dataSource.paginator.firstPage();
     }
     console.log(this.dataSource.data);
-    
+    this.dataSource2 = new MatTableDataSource(site.usage_costs);
+    this.dataSource2.sort = this.sort;
+    this.dataSource2.paginator = this.paginator2;
+    if (this.dataSource2.paginator) {
+      this.dataSource2.paginator.firstPage();
+    }
   }
 }
